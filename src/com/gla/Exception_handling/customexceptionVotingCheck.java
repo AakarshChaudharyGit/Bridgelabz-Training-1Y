50e6a47b9736b1805874adc3e9697ee9475c53d0
AakarshChaudharyGit <aakarshchaudhary.90cse@gmail.com>
2026-03-18 20:29:04 +0530

    work done

---
+++ src/com/gla/Exception_handling/customexceptionVotingCheck.java
@@ -0,0 +1,28 @@
+package com.gla.Exception_handling;
+
+class CustomException extends Exception {
+    CustomException(String message) {
+        super(message);
+    }
+}
+
+public class customexceptionVotingCheck {
+
+    public static void checkVoteValidity(int age) throws CustomException {
+        if (age < 18) {
+            throw new CustomException("Not eligible to vote");
+        } else {
+            System.out.println("Eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        int age = 16;
+
+        try {
+            checkVoteValidity(age);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
