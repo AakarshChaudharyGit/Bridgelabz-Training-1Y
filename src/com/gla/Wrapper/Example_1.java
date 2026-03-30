77c795604749cf656ef106dbffca3298bdf9dcf4
AakarshChaudharyGit <aakarshchaudhary.90cse@gmail.com>
2026-03-30 15:52:44 +0530

    done 30th march work

---
+++ src/com/gla/Wrapper/Example_1.java
@@ -0,0 +1,11 @@
+package com.gla.Wrapper;
+
+public class Example_1 {
+    static void main(String[] args) {
+        int a = 14;
+        Integer b =a;             // Auto- boxing
+        Integer s =new Integer(9);      // boxing
+        int k = s;      // unboxing
+        int x =b;       // Auto-unboxing
+    }
+}
